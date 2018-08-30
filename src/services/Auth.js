e6bc8abaaf918c3e4a754033bd59be94b25fabae
Nikola <milnikola81@gmail.com>
2018-08-30 21:33:16 +0200

    authorization, headers, token etc.

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,28 +1,59 @@
 import axios from 'axios'
 
 export default class AuthService {
-  login(email, password) {
-    return axios.post('http://localhost:8000/api/login', {
-      email, password
-    }).then(data => {
-      window.localStorage.setItem('loginToken', data.data.token)
-      this.setAxiosDefaultAuthorizationHeader()
-    })
-  }
 
-  setAxiosDefaultAuthorizationHeader() {
-    const TOKEN = window.localStorage.getItem('loginToken')
-    axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`
+  constructor() {
+    axios.defaults.baseURL = 'http//localhost:8000/api/'
+    this.setAuthorizationHeader()
   }
 
   logout() {
-    window.localStorage.removeItem('loginToken')
-    delete axios.defaults.headers.common['Authorization']
+    localStorage.removeItem('token')
+    // delete axios.defaults.headers.common['Authorization']
   }
 
+  login(email, password) {
+    return axios.post('auth/login', {
+      email, password
+    }).then((response) => {
+      // console.log(response.data.access_token)
+      window.localStorage.setItem('token', response.data.access_token)
+      this.setAuthorizationHeader()
+    })
+  }
+
+  setAuthorizationHeader() {
+    const token = window.localStorage.getItem('token')
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  }
+
+  
   isAuthenticated() {
-    return !!window.localStorage.getItem('loginToken')
+    return localStorage.getItem('token')
   }
 }
 
 export const authService = new AuthService()
+
+// login(email, password) {
+//   return axios.post('http://localhost:8000/api/login', {
+//     email, password
+//   }).then(data => {
+//     window.localStorage.setItem('loginToken', data.data.token)
+//     this.setAxiosDefaultAuthorizationHeader()
+//   })
+// }
+
+// setAxiosDefaultAuthorizationHeader() {
+//   const TOKEN = window.localStorage.getItem('loginToken')
+//   axios.defaults.headers.common['Authorization'] = `Bearer ${TOKEN}`
+// }
+
+// logout() {
+//   window.localStorage.removeItem('loginToken')
+//   delete axios.defaults.headers.common['Authorization']
+// }
+
+// isAuthenticated() {
+//   return !!window.localStorage.getItem('loginToken')
+// }
